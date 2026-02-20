15d7fbbde9e3b579df5a533bd6d6c7181f6dc2ca
Elif Yeşilyurt <elifyesilyurt000@gmail.com>
2026-02-20 22:22:53 +0300

    dd negative number check and even/odd logic in evenodd.cpp

---
+++ c++/evenodd.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Choose a number: \n";
+    cin >> number;
+
+    if (number < 0)
+    {
+        int number;
+        cout<<"Please enter a positive number.\n";
+        cin >> number;
+    }
+    else
+    {
+        if (number % 2 == 0)
+            cout << number << " is even number.\n";
+        else
+            cout <<number<< " is odd number.\n";
+        
+    }
+    return 0;
+}
