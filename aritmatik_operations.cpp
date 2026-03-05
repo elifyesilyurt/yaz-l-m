b2aeb08b7551027ef40e805b604359a00607a59c
Elif Yeşilyurt <elifyesilyurt000@gmail.com>
2026-03-05 23:08:10 +0300

    add basic arithmetic operations calculator

---
+++ aritmatik_operations.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    double z;
+
+    cout << "Enter two integar: \n";
+    cin >> x;
+    cin >> y;
+    cout << "Enter a double: \n";
+    cin >> z;
+    cout << "Sum: " << x + y << endl;
+    cout << "Difference: " << x - y << endl;
+    cout << "Product: "<< x * y << endl;
+    cout << "Remainder: " << x % y << endl;
+    cout << "Int division: " << x / y << endl;
+    cout << "Float division:" << double(x / y) << endl;
+    cout << "Double x 3: "<< x * 3 << endl;
+
+    return 0;
+}
